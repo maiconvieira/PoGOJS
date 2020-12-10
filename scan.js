739212fd1f22ba40944df1eb6d2b4b0c39195378
Maicon Vieira <maiconrodrigo.vieira@gmail.com>
2020-12-10 13:32:33 -0300

    File of scan pvp stats.

---
+++ scan.js
@@ -0,0 +1,894 @@
+// Ivysaur
+// Venusaur Mega Venusaur
+// Venusaur
+// Charmander
+// Charmeleon
+// Charizard Mega Charizard Y
+// Charizard Mega Charizard X
+// Charizard
+// Squirtle
+// Wartortle
+// Blastoise Mega Blastoise
+// Blastoise
+// Caterpie
+// Metapod
+// Butterfree
+// Weedle
+// Kakuna
+// Beedrill Mega Beedrill
+// Beedrill
+// Pidgey
+// Pidgeotto
+// Pidgeot Mega Pidgeot
+// Pidgeot
+// Rattata Alola Form
+// Rattata
+// Raticate Alola Form
+// Raticate
+// Spearow
+// Fearow
+// Ekans
+// Arbok
+// Pikachu
+// Raichu Alola Form
+// Raichu
+// Sandshrew Alola Form
+// Sandshrew
+// Sandslash Alola Form
+// Sandslash
+// Nidoran♀
+// Nidorina
+// Nidoqueen
+// Nidoran♂
+// Nidorino
+// Nidoking
+// Clefairy
+// Clefable
+// Vulpix Alola Form
+// Vulpix
+// Ninetales Alola Form
+// Ninetales
+// Jigglypuff
+// Wigglytuff
+// Zubat
+// Golbat
+// Oddish
+// Gloom
+// Vileplume
+// Paras
+// Parasect
+// Venonat
+// Venomoth
+// Diglett Alola Form
+// Diglett
+// Dugtrio Alola Form
+// Dugtrio
+// Meowth Alola Form
+// Meowth
+// Meowth Galarian Form
+// Persian Alola Form
+// Persian
+// Psyduck
+// Golduck
+// Mankey
+// Primeape
+// Growlithe
+// Arcanine
+// Poliwag
+// Poliwhirl
+// Poliwrath
+// Abra
+// Kadabra
+// Alakazam Mega Alakazam
+// Alakazam
+// Machop
+// Machoke
+// Machamp
+// Bellsprout
+// Weepinbell
+// Victreebel
+// Tentacool
+// Tentacruel
+// Geodude Alola Form
+// Geodude
+// Graveler Alola Form
+// Graveler
+// Golem Alola Form
+// Golem
+// Ponyta
+// Rapidash
+// Slowpoke
+// Slowbro Mega Slowbro
+// Slowbro
+// Magnemite
+// Magneton
+// Farfetch'd
+// Farfetch'd Galarian Form
+// Doduo
+// Dodrio
+// Seel
+// Dewgong
+// Grimer Alola Form
+// Grimer
+// Muk Alola Form
+// Muk
+// Shellder
+// Cloyster
+// Gastly
+// Haunter
+// Gengar Mega Gengar
+// Gengar
+// Onix
+// Drowzee
+// Hypno
+// Krabby
+// Kingler
+// Voltorb
+// Electrode
+// Exeggcute
+// Exeggutor Alola Form
+// Exeggutor
+// Cubone
+// Marowak Alola Form
+// Marowak
+// Hitmonlee
+// Hitmonchan
+// Lickitung
+// Koffing
+// Weezing
+// Weezing Galarian Form
+// Rhyhorn
+// Rhydon
+// Chansey
+// Tangela
+// Kangaskhan Mega Kangaskhan
+// Kangaskhan
+// Horsea
+// Seadra
+// Goldeen
+// Seaking
+// Staryu
+// Starmie
+// Mr. Mime
+// Scyther
+// Jynx
+// Electabuzz
+// Magmar
+// Pinsir Mega Pinsir
+// Pinsir
+// Tauros
+// Magikarp
+// Gyarados Mega Gyarados
+// Gyarados
+// Lapras
+// Ditto
+// Eevee
+// Vaporeon
+// Jolteon
+// Flareon
+// Porygon
+// Omanyte
+// Omastar
+// Kabuto
+// Kabutops
+// Aerodactyl Mega Aerodactyl
+// Aerodactyl
+// Snorlax
+// Articuno
+// Zapdos
+// Moltres
+// Dratini
+// Dragonair
+// Dragonite
+// Mewtwo Mega Mewtwo Y
+// Mewtwo Mega Mewtwo X
+// Mewtwo
+// Mew
+// Chikorita
+// Bayleef
+// Meganium
+// Cyndaquil
+// Quilava
+// Typhlosion
+// Totodile
+// Croconaw
+// Feraligatr
+// Sentret
+// Furret
+// Hoothoot
+// Noctowl
+// Ledyba
+// Ledian
+// Spinarak
+// Ariados
+// Crobat
+// Chinchou
+// Lanturn
+// Pichu
+// Cleffa
+// Igglybuff
+// Togepi
+// Togetic
+// Natu
+// Xatu
+// Mareep
+// Flaaffy
+// Ampharos Mega Ampharos
+// Ampharos
+// Bellossom
+// Marill
+// Azumarill
+// Sudowoodo
+// Politoed
+// Hoppip
+// Skiploom
+// Jumpluff
+// Aipom
+// Sunkern
+// Sunflora
+// Yanma
+// Wooper
+// Quagsire
+// Espeon
+// Umbreon
+// Murkrow
+// Slowking
+// Misdreavus
+// Unown
+// Wobbuffet
+// Girafarig
+// Pineco
+// Forretress
+// Dunsparce
+// Gligar
+// Steelix Mega Steelix
+// Steelix
+// Snubbull
+// Granbull
+// Qwilfish
+// Scizor Mega Scizor
+// Scizor
+// Shuckle
+// Heracross Mega Heracross
+// Heracross
+// Sneasel
+// Teddiursa
+// Ursaring
+// Slugma
+// Magcargo
+// Swinub
+// Piloswine
+// Corsola
+// Remoraid
+// Octillery
+// Delibird
+// Mantine
+// Skarmory
+// Houndour
+// Houndoom Mega Houndoom
+// Houndoom
+// Kingdra
+// Phanpy
+// Donphan
+// Porygon2
+// Stantler
+// Smeargle
+// Tyrogue
+// Hitmontop
+// Smoochum
+// Elekid
+// Magby
+// Miltank
+// Blissey
+// Raikou
+// Entei
+// Suicune
+// Larvitar
+// Pupitar
+// Tyranitar Mega Tyranitar
+// Tyranitar
+// Lugia
+// Ho-Oh
+// Celebi
+// Treecko
+// Grovyle
+// Sceptile Mega Sceptile
+// Sceptile
+// Torchic
+// Combusken
+// Blaziken Mega Blaziken
+// Blaziken
+// Mudkip
+// Marshtomp
+// Swampert Mega Swampert
+// Swampert
+// Poochyena
+// Mightyena
+// Zigzagoon
+// Zigzagoon Galarian Form
+// Linoone
+// Linoone Galarian Form
+// Wurmple
+// Silcoon
+// Beautifly
+// Cascoon
+// Dustox
+// Lotad
+// Lombre
+// Ludicolo
+// Seedot
+// Nuzleaf
+// Shiftry
+// Taillow
+// Swellow
+// Wingull
+// Pelipper
+// Ralts
+// Kirlia
+// Gardevoir Mega Gardevoir
+// Gardevoir
+// Surskit
+// Masquerain
+// Shroomish
+// Breloom
+// Slakoth
+// Vigoroth
+// Slaking
+// Nincada
+// Ninjask
+// Shedinja
+// Whismur
+// Loudred
+// Exploud
+// Makuhita
+// Hariyama
+// Azurill
+// Nosepass
+// Skitty
+// Delcatty
+// Sableye Mega Sableye
+// Sableye
+// Mawile Mega Mawile
+// Mawile
+// Aron
+// Lairon
+// Aggron Mega Aggron
+// Aggron
+// Meditite
+// Medicham Mega Medicham
+// Medicham
+// Electrike
+// Manectric Mega Manectric
+// Manectric
+// Plusle
+// Minun
+// Volbeat
+// Illumise
+// Roselia
+// Gulpin
+// Swalot
+// Carvanha
+// Sharpedo Mega Sharpedo
+// Sharpedo
+// Wailmer
+// Wailord
+// Numel
+// Camerupt Mega Camerupt
+// Camerupt
+// Torkoal
+// Spoink
+// Grumpig
+// Spinda
+// Trapinch
+// Vibrava
+// Flygon
+// Cacnea
+// Cacturne
+// Swablu
+// Altaria Mega Altaria
+// Altaria
+// Zangoose
+// Seviper
+// Lunatone
+// Solrock
+// Barboach
+// Whiscash
+// Corphish
+// Crawdaunt
+// Baltoy
+// Claydol
+// Lileep
+// Cradily
+// Anorith
+// Armaldo
+// Feebas
+// Milotic
+// Castform Sunny Form
+// Castform Snowy Form
+// Castform Rainy Form
+// Castform
+// Kecleon
+// Shuppet
+// Banette Mega Banette
+// Banette
+// Duskull
+// Dusclops
+// Tropius
+// Chimecho
+// Absol Mega Absol
+// Absol
+// Wynaut
+// Snorunt
+// Glalie Mega Glalie
+// Glalie
+// Spheal
+// Sealeo
+// Walrein
+// Clamperl
+// Huntail
+// Gorebyss
+// Relicanth
+// Luvdisc
+// Bagon
+// Shelgon
+// Salamence Mega Salamence
+// Salamence
+// Beldum
+// Metang
+// Metagross Mega Metagross
+// Metagross
+// Regirock
+// Regice
+// Registeel
+// Latias Mega Latias
+// Latias
+// Latios Mega Latios
+// Latios
+// Kyogre Primal Reversion
+// Kyogre
+// Groudon Primal Reversion
+// Groudon
+// Rayquaza Mega Rayquaza
+// Rayquaza
+// Jirachi
+// Deoxys Speed Forme
+// Deoxys Defense Forme
+// Deoxys Attack Forme
+// Deoxys
+// Turtwig
+// Grotle
+// Torterra
+// Chimchar
+// Monferno
+// Infernape
+// Piplup
+// Prinplup
+// Empoleon
+// Starly
+// Staravia
+// Staraptor
+// Bidoof
+// Bibarel
+// Kricketot
+// Kricketune
+// Shinx
+// Luxio
+// Luxray
+// Budew
+// Roserade
+// Cranidos
+// Rampardos
+// Shieldon
+// Bastiodon
+// Burmy
+// Wormadam Trash Cloak
+// Wormadam Sandy Cloak
+// Wormadam
+// Mothim
+// Combee
+// Vespiquen
+// Pachirisu
+// Buizel
+// Floatzel
+// Cherubi
+// Cherrim
+// Shellos
+// Gastrodon
+// Ambipom
+// Drifloon
+// Drifblim
+// Buneary
+// Lopunny Mega Lopunny
+// Lopunny
+// Mismagius
+// Honchkrow
+// Glameow
+// Purugly
+// Chingling
+// Stunky
+// Skuntank
+// Bronzor
+// Bronzong
+// Bonsly
+// Mime Jr.
+// Happiny
+// Chatot
+// Spiritomb
+// Gible
+// Gabite
+// Garchomp Mega Garchomp
+// Garchomp
+// Munchlax
+// Riolu
+// Lucario Mega Lucario
+// Lucario
+// Hippopotas
+// Hippowdon
+// Skorupi
+// Drapion
+// Croagunk
+// Toxicroak
+// Carnivine
+// Finneon
+// Lumineon
+// Mantyke
+// Snover
+// Abomasnow Mega Abomasnow
+// Abomasnow
+// Weavile
+// Magnezone
+// Lickilicky
+// Rhyperior
+// Tangrowth
+// Electivire
+// Magmortar
+// Togekiss
+// Yanmega
+// Leafeon
+// Glaceon
+// Gliscor
+// Mamoswine
+// Porygon-Z
+// Gallade Mega Gallade
+// Gallade
+// Probopass
+// Dusknoir
+// Froslass
+// Rotom Wash Rotom
+// Rotom Mow Rotom
+// Rotom Heat Rotom
+// Rotom Frost Rotom
+// Rotom Fan Rotom
+// Rotom
+// Uxie
+// Mesprit
+// Azelf
+// Dialga
+// Palkia
+// Heatran
+// Regigigas
+// Giratina Origin Forme
+// Giratina
+// Cresselia
+// Phione
+// Manaphy
+// Darkrai
+// Shaymin Sky Forme
+// Shaymin
+// Arceus
+// Victini
+// Snivy
+// Servine
+// Serperior
+// Tepig
+// Pignite
+// Emboar
+// Oshawott
+// Dewott
+// Samurott
+// Patrat
+// Watchog
+// Lillipup
+// Herdier
+// Stoutland
+// Purrloin
+// Liepard
+// Pansage
+// Simisage
+// Pansear
+// Simisear
+// Panpour
+// Simipour
+// Munna
+// Musharna
+// Pidove
+// Tranquill
+// Unfezant
+// Blitzle
+// Zebstrika
+// Roggenrola
+// Boldore
+// Gigalith
+// Woobat
+// Swoobat
+// Drilbur
+// Excadrill
+// Audino Mega Audino
+// Audino
+// Timburr
+// Gurdurr
+// Conkeldurr
+// Tympole
+// Palpitoad
+// Seismitoad
+// Throh
+// Sawk
+// Sewaddle
+// Swadloon
+// Leavanny
+// Venipede
+// Whirlipede
+// Scolipede
+// Cottonee
+// Whimsicott
+// Petilil
+// Lilligant
+// Basculin Blue-Striped Form
+// Basculin
+// Sandile
+// Krokorok
+// Krookodile
+// Darumaka
+// Darumaka Galarian Form
+// Darmanitan Zen Mode
+// Darmanitan Galarian Form Zen Mode
+// Darmanitan
+// Maractus
+// Dwebble
+// Crustle
+// Scraggy
+// Scrafty
+// Sigilyph
+// Yamask
+// Cofagrigus
+// Tirtouga
+// Carracosta
+// Archen
+// Archeops
+// Trubbish
+// Garbodor
+// Zorua
+// Zoroark
+// Minccino
+// Cinccino
+// Gothita
+// Gothorita
+// Gothitelle
+// Solosis
+// Duosion
+// Reuniclus
+// Ducklett
+// Swanna
+// Vanillite
+// Vanillish
+// Vanilluxe
+// Deerling
+// Sawsbuck
+// Emolga
+// Karrablast
+// Escavalier
+// Foongus
+// Amoonguss
+// Frillish
+// Jellicent
+// Alomomola
+// Joltik
+// Galvantula
+// Ferroseed
+// Ferrothorn
+// Klink
+// Klang
+// Klinklang
+// Tynamo
+// Eelektrik
+// Eelektross
+// Elgyem
+// Beheeyem
+// Litwick
+// Lampent
+// Chandelure
+// Axew
+// Fraxure
+// Haxorus
+// Cubchoo
+// Beartic
+// Cryogonal
+// Shelmet
+// Accelgor
+// Stunfisk
+// Stunfisk Galarian Form
+// Mienfoo
+// Mienshao
+// Druddigon
+// Golett
+// Golurk
+// Pawniard
+// Bisharp
+// Bouffalant
+// Rufflet
+// Braviary
+// Vullaby
+// Mandibuzz
+// Heatmor
+// Durant
+// Deino
+// Zweilous
+// Hydreigon
+// Larvesta
+// Volcarona
+// Cobalion
+// Terrakion
+// Virizion
+// Tornadus Therian Forme
+// Tornadus
+// Thundurus Therian Forme
+// Thundurus
+// Reshiram
+// Zekrom
+// Landorus Therian Forme
+// Landorus
+// Kyurem White Kyurem
+// Kyurem Black Kyurem
+// Kyurem
+// Keldeo Resolute Form
+// Keldeo
+// Meloetta Pirouette Forme
+// Meloetta
+// Genesect
+// Chespin
+// Quilladin
+// Chesnaught
+// Fennekin
+// Braixen
+// Delphox
+// Froakie
+// Frogadier
+// Greninja
+// Bunnelby
+// Diggersby
+// Fletchling
+// Fletchinder
+// Talonflame
+// Scatterbug
+// Spewpa
+// Vivillon
+// Litleo
+// Pyroar
+// Flabébé
+// Floette Eternal Flower
+// Floette
+// Florges
+// Skiddo
+// Gogoat
+// Pancham
+// Pangoro
+// Furfrou
+// Espurr
+// Meowstic Female
+// Meowstic
+// Honedge
+// Doublade
+// Aegislash Blade Forme
+// Aegislash
+// Spritzee
+// Aromatisse
+// Swirlix
+// Slurpuff
+// Inkay
+// Malamar
+// Binacle
+// Barbaracle
+// Skrelp
+// Dragalge
+// Clauncher
+// Clawitzer
+// Helioptile
+// Heliolisk
+// Tyrunt
+// Tyrantrum
+// Amaura
+// Aurorus
+// Sylveon
+// Hawlucha
+// Dedenne
+// Carbink
+// Goomy
+// Sliggoo
+// Goodra
+// Klefki
+// Phantump
+// Trevenant
+// Pumpkaboo Super Size
+// Pumpkaboo Small Size
+// Pumpkaboo Large Size
+// Pumpkaboo
+// Gourgeist Super Size
+// Gourgeist Small Size
+// Gourgeist Large Size
+// Gourgeist
+// Bergmite
+// Avalugg
+// Noibat
+// Noivern
+// Xerneas
+// Yveltal
+// Zygarde
+// Diancie Mega Diancie
+// Diancie
+// Hoopa Hoopa Unbound
+// Hoopa
+// Volcanion
+// Meltan
+// Melmetal
+// Sirfetch’d
+
+function statsBulbasaur(objeto) {
+    if (dict.attack <= 15 && dict.defence <= 15 && dict.stamina <= 15 && dict.level <= 40) {
+        console.log(JSON.stringify(pokeDict[pk.id]["name"], null, 4));
+    // text += "Ivysaur (" + pokeDict[pk.id]["name"] + "): \t" + pk.level + " - " + pk.cp + "\t" + pk.center.lat + "," + pk.center.lng + "\n\r";
+  // } else if (statsBulbasaur.attack == 0 && statsBulbasaur.defence == 14 && statsBulbasaur.stamina == 11 && statsBulbasaur.level <= 21) {
+  //   console.log(JSON.stringify(dict, null, 4));
+    } else {
+        console.log(JSON.stringify('Bulbasaur falhou', null, 4));
+    }
+}
+function statsCharmander(objeto) {
+    if (dict.attack <= 15 && dict.defence <= 15 && dict.stamina <= 15 && dict.level <= 40) {
+        console.log(JSON.stringify(dict, null, 4));
+  // } else if (statsCharmander.attack == 0 && statsCharmander.defence == 14 && statsCharmander.stamina == 11 && statsCharmander.level <= 21) {
+  //   console.log(JSON.stringify(dict, null, 4));
+    } else {
+        console.log(JSON.stringify('Charmander falhou', null, 4));
+    }
+}
+function statsSquirtle(objeto) {
+  if (statsSquirtle.attack <= 15 && statsSquirtle.defence <= 15 && statsSquirtle.stamina <= 15 && statsSquirtle.level <= 40) {
+    console.log(JSON.stringify('OK', null, 4));
+ // } else if (statsSquirtle.attack == 0 && statsSquirtle.defence == 14 && statsSquirtle.stamina == 11 && statsSquirtle.level <= 21) {
+ //   console.log(JSON.stringify(dict, null, 4));
+  } else {
+        console.log(JSON.stringify('Squirtle falhou', null, 4));
+    }
+}
+
+var i = 0;
+for (; i < pokemons.length; ) {
+    if (pokemons[i].attack != -1) {
+        var dict = {};
+        dict.id = pokemons[i].id;
+        dict.name = pokeDict[pokemons[i].id]["name"];
+        dict.form = pokemons[i].form;
+        dict.gender = pokemons[i].gender;
+        dict.attack = pokemons[i].attack;
+        dict.defence = pokemons[i].defence;
+        dict.stamina = pokemons[i].attack;
+        dict.level = pokemons[i].stamina;
+        dict.cp = pokemons[i].cp;
+        dict.lat = pokemons[i].center.lat;
+        dict.lng = pokemons[i].center.lng;
+        i++;
+        if (dict.id == 1) { statsBulbasaur(dict) }
+        else if (dict.id == 4) { statsCharmander(dict) }
+        else if (dict.id == 7) { statsSquirtle(dict) }
+    }
+    i++;
+}
+
+function minhaFuncao(objeto) {
+  objeto.make = "Toyota";
+}
+var meucarro = {make: "Honda", model: "Accord", year: 1998};
+var x, y;
+x = meucarro.make;     // x recebe o valor "Honda"
+minhaFuncao(meucarro);
+y = meucarro.make;     // y recebe o valor "Toyota" - (a propriedade make foi alterada pela função)
